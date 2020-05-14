1503d25745b771f45d3cb34ab8da52ffb7974ebe
tutorialflutter <nyubiflutter@gmail.com>
2020-05-14 14:17:58 +0700

    Signed-off-by: tutorialflutter <nyubiflutter@gmail.com>

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Row Column'),),
+        body: Center(child: Text("Column"),)
+      ),
+    )
+  }
+}
